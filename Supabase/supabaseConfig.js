b32093682230ec4975bd0aa4d638e66675f2345e
Legend6391 <udhaykiran741@gmail.com>
2026-04-04 19:11:27 +0530

    Delete Supabase directory

--- Supabase/supabaseConfig.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-// Replace these with your actual Supabase URL and Anon Key found in the Project Settings -> API
-const supabaseUrl = "https://xirpynrdafqrsrbfcfyh.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpcnB5bnJkYWZxcnNyYmZjZnloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4OTE5MjcsImV4cCI6MjA4OTQ2NzkyN30.xuJlANpD7wC-9FxCPQ_Po0XtNE2AqehT3UQEPODVbzI";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
